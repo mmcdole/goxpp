77e4a51a73ed99ee3f33c1474dc166866304acbd
Daniel Low <daniellowtw@gmail.com>
2017-07-20 09:07:51 +0100

    Add a stack for the namespaces

--- xpp_test.go
+++ xpp_test.go
@@ -1,6 +1,8 @@
 package xpp_test
 
 import (
+	"bytes"
+	"io"
 	"testing"
 
 	"github.com/mmcdole/goxpp"
@@ -28,4 +30,44 @@ func TestEventName(t *testing.T) {
 		actual := p.EventName(test.event)
 		assert.Equal(t, actual, test.expected, "Expect event name %s did not match actual event name %s.\n", test.expected, actual)
 	}
+}
+
+func TestSpaceStackSelfClosingTag(t *testing.T) {
+	crReader := func(charset string, input io.Reader) (io.Reader, error) {
+		return input, nil
+	}
+	r := bytes.NewBufferString(`<a:y xmlns:a="z"/><x>foo</x>`)
+	p := xpp.NewXMLPullParser(r, false, crReader)
+	toNextStart(t, p)
+	assert.EqualValues(t, map[string]string{"z": "a"}, p.Spaces)
+	toNextStart(t, p)
+	assert.EqualValues(t, map[string]string{}, p.Spaces)
+}
+
+func TestSpaceStackNestedTag(t *testing.T) {
+	crReader := func(charset string, input io.Reader) (io.Reader, error) {
+		return input, nil
+	}
+	r := bytes.NewBufferString(`<y xmlns:a="z"><a:x>foo</a:x></y><w></w>`)
+	p := xpp.NewXMLPullParser(r, false, crReader)
+	toNextStart(t, p)
+	assert.EqualValues(t, map[string]string{"z": "a"}, p.Spaces)
+	toNextStart(t, p)
+	assert.EqualValues(t, map[string]string{"z": "a"}, p.Spaces)
+	toNextStart(t, p)
+	assert.EqualValues(t, map[string]string{}, p.Spaces)
+}
+
+func toNextStart(t *testing.T, p *xpp.XMLPullParser) {
+	for {
+		tok, err := p.NextToken()
+		if err != nil {
+			t.Error(err)
+			t.FailNow()
+		}
+		if tok == xpp.StartTag {
+			break
+		}
+	}
+	return
 }
